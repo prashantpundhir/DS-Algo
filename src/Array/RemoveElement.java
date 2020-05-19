ffb03c62e3111762d499da1350328f02f3fb827b
PraSha <PraSha@192.168.100.12>
2020-05-20 00:03:56 +0530

    add duplicatezeros, merge2sortedarray,removeelement, trapping rain water

---
+++ src/Array/RemoveElement.java
@@ -0,0 +1,36 @@
+package Array;
+
+public class RemoveElement {
+
+	public static void main(String[] args) {
+
+		int arr[]= {1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7};
+		int val = 7;
+		
+System.out.println(removeEle(arr, val));
+		
+	}
+
+	public static int removeEle(int arr[], int val) {
+		
+		int i=0;
+		int n = arr.length;
+		while(i<n) {
+			if(arr[i]== val) {
+				arr[i] = arr[n-1];
+				n--;
+			}
+			else
+				i++;
+			
+			
+		}
+		
+		for(int j=0;j<n;j++)
+			System.out.print(arr[j]);
+		System.out.println("");
+		return n;
+		
+	}
+}
+
