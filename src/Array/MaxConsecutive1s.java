78d470eb291cdd40570925b32722eff8c7bc518d
PraSha <PraSha@192.168.100.12>
2020-05-20 23:33:36 +0530

    mountain Array

--- src/Array/MaxConsecutive1s.java
+++ src/Array/MaxConsecutive1s.java
@@ -9,8 +9,8 @@ public class MaxConsecutive1s {
 	}
 
 	public static int findMaxConsecutiveOnes(int[] nums) {
-		int count = 0, countZero=0;;
-		int max = 0, maxZero = 0;
+		int count = 0 ;
+		int max = 0;
 		for (int i = 0; i < nums.length; i++) {
 			if (nums[i] == 0) {
 				count = 0;
