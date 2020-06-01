9c3dd04af9b0fefd142aff6d1a76864b449cb848
PraSha <PraSha@192.168.100.12>
2020-06-01 22:56:48 +0530

    find max 1s, duplicate remove

---
+++ src/Array/FindRowWithMax1s.java
@@ -0,0 +1,27 @@
+package Array;
+
+public class FindRowWithMax1s {
+
+	public static void main(String[] args) {
+
+		int arr[][] = { { 0, 0, 0, 1, 1 },
+				{ 0, 0, 0, 0, 1},
+				{ 0, 0, 0, 0, 1 },
+				{ 0, 0, 1, 1, 1 },
+				{ 0, 1, 1, 1, 1 } ,
+				{ 0, 0, 1, 1, 1}};
+		
+		findMax1s(arr);
+
+	}
+	
+	public static void findMax1s(int [][]arr) {
+		int rows = arr.length;
+		
+		
+		
+		
+	}
+	
+
+}
