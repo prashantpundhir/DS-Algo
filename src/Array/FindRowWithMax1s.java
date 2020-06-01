a2f3d1963a88c30b58c6ea11d752b8c4c9d97b9d
PraSha <PraSha@192.168.100.12>
2020-06-01 23:11:41 +0530

    find row with max 1s

--- src/Array/FindRowWithMax1s.java
+++ src/Array/FindRowWithMax1s.java
@@ -4,24 +4,28 @@ public class FindRowWithMax1s {
 
 	public static void main(String[] args) {
 
-		int arr[][] = { { 0, 0, 0, 1, 1 },
-				{ 0, 0, 0, 0, 1},
-				{ 0, 0, 0, 0, 1 },
-				{ 0, 0, 1, 1, 1 },
-				{ 0, 1, 1, 1, 1 } ,
-				{ 0, 0, 1, 1, 1}};
-		
-		findMax1s(arr);
+		int arr[][] = { { 0, 0, 1, 1, 1 }, { 0, 0, 0, 0, 1 }, { 0, 0, 0, 0, 1 }, { 0, 0, 1, 1, 1 }, { 0, 1, 1, 1, 1 },
+				{ 0, 0, 1, 1, 1 } };
+
+		System.out.println(findMax1s(arr));
 
 	}
-	
-	public static void findMax1s(int [][]arr) {
-		int rows = arr.length;
-		
-		
-		
-		
+
+	public static int findMax1s(int[][] arr) {
+		int n = arr.length;
+		int col = n == 0 ? 0 : arr[0].length;
+		int j = col - 1;
+		int i = 0, row = 0;
+
+		while (i < n && j >= 0) {
+
+			if (arr[i][j] == 1) {
+				row = i;
+				j--;
+			} else
+				i++;
+		}
+		return row;
 	}
-	
 
 }
