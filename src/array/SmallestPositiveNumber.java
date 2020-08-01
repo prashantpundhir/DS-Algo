fbdb41a39a00f1b8d3927b965c2fd349d15d9477
PraSha <PraSha@192.168.100.8>
2020-08-01 19:08:34 +0530

    smallest positive number

--- src/array/SmallestPositiveNumber.java
+++ src/array/SmallestPositiveNumber.java
@@ -4,35 +4,31 @@ public class SmallestPositiveNumber {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 4,5,2,1 }; // output: 3
-
+		int[] arr = { 7, 6, 1, 2, 5, 4, 3, 9, 8, 12, 15, 10 }; // output: 3,
 		System.out.println(smallestPositiveNumber(arr));
 
 	}
 
-	public static int smallestPositiveNumber(int []arr) {
-		int num=0;
-		
-		for(int i=0;i<arr.length;i++) {
-			int n = arr[i]-1;
-			if(n>=0 && n<arr.length)
-				arr[n]*=-1;
+	public static int smallestPositiveNumber(int[] arr) {
+		int num = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			int n = Math.abs(arr[i]) - 1;
+			if (n >= 0 && n < arr.length)
+				arr[n] = -arr[n];
+
 		}
-		
-		for(int i: arr)
-			System.out.println(i);
-		
-		
-		for(int i=0;i<arr.length;i++) {
-			if(arr[i]>0) {
-				num=i+1;	
-			break;
-			}		
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > 0) {
+				num = i + 1;
+				break;
+			}
 		}
-		
-		if(num==0)
-			return arr.length+1;
+
+		if (num == 0)
+			num = arr.length + 1;
 		return num;
 	}
-	
+
 }
