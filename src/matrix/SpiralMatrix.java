28f6b31691d8952d4e0abf6b330212453307b71f
PraSha <PraSha@LAPTOP-AEBGC043>
2020-09-29 20:34:28 +0530

    spiral matrix

--- src/matrix/SpiralMatrix.java
+++ src/matrix/SpiralMatrix.java
@@ -8,8 +8,12 @@ public class SpiralMatrix {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int arr[][] = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 }, { 16, 17, 18, 19, 20 },
-				{ 21, 22, 23, 24, 25 }, { 26, 27, 28, 29, 30 } };
+		int arr[][] = { { 1, 2, 3, 4, 5 }, 
+						{ 6, 7, 8, 9, 10 }, 
+						{ 11, 12, 13, 14, 15 }, 
+						{ 16, 17, 18, 19, 20 },
+						{ 21, 22, 23, 24, 25 }, 
+						{ 26, 27, 28, 29, 30 } };
 
 		List<Integer> li = spiralMatrix(arr);
 		System.out.println(li);
